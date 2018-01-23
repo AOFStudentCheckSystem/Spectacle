01e6ba1dcdd1b23329f66bd9942e7890e283e269
Codetector <codetector@codetector.cn>
2018-01-22 21:23:02 -0500

    Hummm check-in working? I guess. should be at least some what.

--- app/src/renderer/components/Event/CheckPage.vue
+++ app/src/renderer/components/Event/CheckPage.vue
@@ -2,101 +2,107 @@
 * Created by dummy on 4/14/17.
 */
 <style scoped>
-  .remove-top-margin {
-    margin-top: 0 !important;
-  }
+    .remove-top-margin {
+        margin-top: 0 !important;
+    }
 
-  .remove-all-margin {
-    margin: 0 !important;
-  }
+    .remove-all-margin {
+        margin: 0 !important;
+    }
 </style>
 
 <template>
-  <f7-page name="event-check" @page:init="setPageActive(true)"
-           @page:reinit="setPageActive(true)" @page:beforeremove="setPageActive(false)">
-    <f7-navbar :title="computedTitle" back-link="Back" sliding>
-      <f7-nav-right>
-        <f7-link @click="stupidKidForgotHisCard" v-if="currentEvent && currentEvent.status < 2">
-          <f7-icon v-if="remove" f7="delete_round"></f7-icon>
-          <f7-icon v-else f7="add_round"></f7-icon>
-        </f7-link>
-      </f7-nav-right>
-    </f7-navbar>
+    <f7-page name="event-check" @page:init="setPageActive(true)"
+             @page:reinit="setPageActive(true)" @page:beforeremove="setPageActive(false)">
+        <f7-navbar :title="computedTitle" back-link="Back" sliding>
+            <f7-nav-right>
+                <f7-link @click="stupidKidForgotHisCard()   " v-if="currentEvent && isUnlockedEvent(currentEvent) ">
+                    <f7-icon v-if="remove" f7="delete_round"></f7-icon>
+                    <f7-icon v-else f7="add_round"></f7-icon>
+                </f7-link>
+            </f7-nav-right>
+        </f7-navbar>
 
-    <search-bar v-model="filter" @refresh="$refs['virtualscroller'].updateVisibleItems()" @overlayActive="overlayActive = $event"></search-bar>
-    <search-bar-overlay :active="overlayActive"></search-bar-overlay>
+        <search-bar v-model="filter" @refresh="$refs['virtualscroller'].updateVisibleItems()"
+                    @overlayActive="overlayActive = $event"></search-bar>
+        <search-bar-overlay :active="overlayActive"></search-bar-overlay>
 
-    <f7-block>
-      <f7-grid>
-        <f7-col :width="75 + !displayPhoto * 25">
-          <f7-list no-hairlines no-hairlines-between inset class="remove-all-margin">
-            <f7-list-item :title="'Records: ' + amountRecords"></f7-list-item>
-            <f7-list-item :title="'Add: ' + amountAdd"></f7-list-item>
-            <f7-list-item :title="'Remove: ' + amountRemove"></f7-list-item>
-            <li class="media-item" v-if="currentEvent && currentEvent.status < 2">
-              <div class="item-content">
-                <div class="item-inner">
-                  <div class="item-title-row">
-                    <div class="item-title">Scan To Remove</div>
-                    <div class="item-after">
-                      <f7-input type="switch" @input="setRemoveValue" :value="remove"></f7-input>
+        <f7-block>
+            <f7-grid>
+                <f7-col :width="75 + !displayPhoto * 25">
+                    <f7-list no-hairlines no-hairlines-between inset class="remove-all-margin">
+                        <f7-list-item :title="'Records: ' + amountRecords"></f7-list-item>
+                        <f7-list-item :title="'Add: ' + amountAdd"></f7-list-item>
+                        <f7-list-item :title="'Remove: ' + amountRemove"></f7-list-item>
+                        <li class="media-item" v-if="currentEvent && currentEvent.status < 2">
+                            <div class="item-content">
+                                <div class="item-inner">
+                                    <div class="item-title-row">
+                                        <div class="item-title">Scan To Remove</div>
+                                        <div class="item-after">
+                                            <f7-input type="switch" @input="setRemoveValue" :value="remove"></f7-input>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    </f7-list>
+                </f7-col>
+                <f7-col v-if="displayPhoto" :width="25">
+                    <f7-block inset class="remove-all-margin">Reserved</f7-block>
+                </f7-col>
+            </f7-grid>
+        </f7-block>
+
+        <virtual-scroller ref="virtualscroller" v-show="filteredCurrentEventRecords.length > 0" id="check-list"
+                          containerTag="ul" mainTag="div"
+                          :class="['list-block', 'media-list', 'check-searchbar-found', 'tablet-inset']"
+                          :items="filteredCurrentEventRecords" :itemHeight="63" keyField="id">
+            <template scope="props">
+                <li class="swipeout"
+                    :key="props.itemKey">
+                    <div class="swipeout-content">
+                        <div class="item-content">
+                            <div class="item-inner">
+                                <div class="item-title-row">
+                                    <div class="item-title">{{props.item.student.lastName + ', ' +
+                                        props.item.student.firstName}}
+                                    </div>
+                                    <div class="item-after"><span class="badge"
+                                                                  :class="props.item.checkInTime >= 0 ? 'color-blue' : 'color-red'">{{props.item.checkInTime >= 0 ? 'Checked' : 'Removed'}}</span>
+                                    </div>
+                                </div>
+                                <div class="item-subtitle">{{props.item.student.preferredName ||
+                                    props.item.student.firstName}}
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                  </div>
-                </div>
-              </div>
-            </li>
-          </f7-list>
-        </f7-col>
-        <f7-col v-if="displayPhoto" :width="25">
-          <f7-block inset class="remove-all-margin">Reserved</f7-block>
-        </f7-col>
-      </f7-grid>
-    </f7-block>
+                    <!--<div class="swipeout-actions-left" v-if="props.item.checkInTime < 0">-->
+                    <!--<a class="swipeout-close swipeout-overswipe bg-blue" href="#">Add</a>-->
+                    <!--</div>-->
+                    <!--<div class="swipeout-actions-right" v-if="props.item.checkInTime >= 0">-->
+                    <!--<a class="swipeout-close swipeout-overswipe bg-red" href="#">Remove</a>-->
+                    <!--</div>-->
+                    <f7-swipeout-actions right v-if="props.item.checkInTime >= 0 && currentEvent.status < 2">
+                        <f7-swipeout-button close overswipe color="red" @click="removeSwiped(props.item.student)">Remove
+                        </f7-swipeout-button>
+                    </f7-swipeout-actions>
+                    <f7-swipeout-actions left v-if="props.item.checkInTime < 0 && currentEvent.status < 2">
+                        <f7-swipeout-button close overswipe color="blue" @click="addSwiped(props.item.student)">Add
+                        </f7-swipeout-button>
+                    </f7-swipeout-actions>
+                </li>
+            </template>
+        </virtual-scroller>
 
-    <virtual-scroller ref="virtualscroller" v-show="filteredCurrentEventRecords.length > 0" id="check-list" containerTag="ul" mainTag="div"
-                      :class="['list-block', 'media-list', 'check-searchbar-found', 'tablet-inset']"
-                      :items="filteredCurrentEventRecords" :itemHeight="63" keyField="id">
-      <template scope="props">
-        <li class="swipeout"
-            :key="props.itemKey">
-          <div class="swipeout-content">
-            <div class="item-content">
-              <div class="item-inner">
-                <div class="item-title-row">
-                  <div class="item-title">{{props.item.student.lastName + ', ' + props.item.student.firstName}}</div>
-                  <div class="item-after"><span class="badge"
-                                                :class="props.item.checkInTime >= 0 ? 'color-blue' : 'color-red'">{{props.item.checkInTime >= 0 ? 'Checked' : 'Removed'}}</span>
-                  </div>
-                </div>
-                <div class="item-subtitle">{{props.item.student.preferredName || props.item.student.firstName}}</div>
-              </div>
-            </div>
-          </div>
-          <!--<div class="swipeout-actions-left" v-if="props.item.checkInTime < 0">-->
-          <!--<a class="swipeout-close swipeout-overswipe bg-blue" href="#">Add</a>-->
-          <!--</div>-->
-          <!--<div class="swipeout-actions-right" v-if="props.item.checkInTime >= 0">-->
-          <!--<a class="swipeout-close swipeout-overswipe bg-red" href="#">Remove</a>-->
-          <!--</div>-->
-          <f7-swipeout-actions right v-if="props.item.checkInTime >= 0 && currentEvent.status < 2">
-            <f7-swipeout-button close overswipe color="red" @click="removeSwiped(props.item.student)">Remove
-            </f7-swipeout-button>
-          </f7-swipeout-actions>
-          <f7-swipeout-actions left v-if="props.item.checkInTime < 0 && currentEvent.status < 2">
-            <f7-swipeout-button close overswipe color="blue" @click="addSwiped(props.item.student)">Add
-            </f7-swipeout-button>
-          </f7-swipeout-actions>
-        </li>
-      </template>
-    </virtual-scroller>
-
-  </f7-page>
+    </f7-page>
 </template>
 
 <script>
     import {mapGetters} from 'vuex'
     import {SmartCardController} from 'smartcard'
-    import {ActivityEventRecord} from '../../models/event'
+    import {ActivityEventRecord, EventStatus} from '../../models/event'
     import {EventBusMixin} from '../../mixins/event-bus'
     import SearchBar from '../Master/SearchBar.vue'
     import SearchBarOverlay from '../Master/SearchBarOverlay.vue'
@@ -128,7 +134,7 @@
                 return this.currentEvent ? this.currentEvent.name : ''
             },
             checkable () {
-                return this.currentEvent ? this.currentEvent.status < 2 : false
+                return this.currentEvent ? this.isUnlockedEvent(this.currentEvent) : false
             },
             amountRecords () {
                 return this.sortedCurrentEventRecords.length
@@ -155,6 +161,9 @@
         methods: {
             setRemoveValue (event) {
                 this.remove = event
+            },
+            isUnlockedEvent (event) {
+                return event.status !== EventStatus.COMPLETED
             },
             setPageActive (active) {
                 this.pageActive = active
--- app/src/renderer/models/student.js
+++ app/src/renderer/models/student.js
@@ -4,12 +4,30 @@
 export class Student {
     constructor (json) {
         this.idNumber = json.idNumber
-        this.lastName = json.account.lastName
-        this.firstName = json.account.firstName
-        this.preferredName = json.account.preferredName
         this.cardSecret = json.cardSecret
-        this.email = json.account.email
+        this.account = new Account(json.account)
         this.dirty = !!json.dirty
         this.cardSecretUpdate = !!json.cardSecretUpdate
+    }
+    get lastName () {
+        return this.account.lastName
+    }
+    get firstName () {
+        return this.account.firstName
+    }
+    get email () {
+        return this.account.email
+    }
+    get preferredName () {
+        return this.account.preferredName
+    }
+}
+
+export class Account {
+    constructor (json) {
+        this.lastName = json.lastName
+        this.firstName = json.firstName
+        this.email = json.email
+        this.preferredName = json.preferredName
     }
 }
